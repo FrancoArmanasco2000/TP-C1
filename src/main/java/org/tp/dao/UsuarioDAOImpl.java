c4e287972235c5fca4d03d6ece68f5899fc1f130
FrancoArmanasco2000 <programmingarmanasco@gmail.com>
2024-11-04 19:19:05 -0300

    Add getBedelByUsuario

--- src/main/java/org/tp/dao/UsuarioDAOImpl.java
+++ src/main/java/org/tp/dao/UsuarioDAOImpl.java
@@ -2,8 +2,13 @@ package org.tp.dao;
 
 import org.tp.entity.Bedel;
 
+import java.util.List;
+
 public interface UsuarioDAOImpl {
 
     public void crearUsuario(Bedel bedel);
 
+    public List<String> listaNombreUsuarios();
+
+    public Bedel getBedelByUsuario(String usuario);
 }
