6cad7e3b24da2896342540841f83b4cf63f77949
mariopo3 <mariopoccia333@gmail.com>
2024-12-19 13:34:29 -0300

    Se agregó nombres con acentos y ñ como válido en varios campos. Se agregó el diálogo de que el boton Buscar Aula no funciona porque no se debía implementar.

--- src/main/java/org/tp/utils/InterfazUtils.java
+++ src/main/java/org/tp/utils/InterfazUtils.java
@@ -12,19 +12,19 @@ public class InterfazUtils {
         }
         // Nombre y Apellido invalido
 
-        if (!nombreApellidoTexto.matches("[A-Za-z]+\\s[A-Za-z]+")) {
+        if (!nombreApellidoTexto.matches("[A-ZÑÁÉÍÓÚa-záéíóú]+\\s[A-ZÑÁÉÍÓÚa-záéíóú]+")) {
             JOptionPane.showMessageDialog(null, "El campo Apellido y Nombre debe tener formato: Apellido Nombre.", "Error de Validación", JOptionPane.ERROR_MESSAGE);
             return false;
         }
         // Asignatura tiene que ser una sola palabra
 
-        if (!asignaturaTexto.matches("[A-Zña-zÑ]*")) {
+        if (!asignaturaTexto.matches("[A-ZÑÁÉÍÓÚa-zñáéíóú]*")) {
             JOptionPane.showMessageDialog(null, "El campo Asignatura debe ser una sola palabra.", "Error de Validación", JOptionPane.ERROR_MESSAGE);
             return false;
         }
         // Formato de email
 
-        if (!correoTexto.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")) {
+        if (!correoTexto.matches("[A-ZÑÁÉÍÓÚa-zñáéíóú0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")) {
             JOptionPane.showMessageDialog(null, "El correo electrónico debe tener un formato válido, como ejemplo@dominio.com.", "Error de Validación", JOptionPane.ERROR_MESSAGE);
             return false;
         }
